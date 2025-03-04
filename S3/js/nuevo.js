f4010f5f40ea58ab27ed4a1455fff85eacd163d2
Jonathantg94 <94640607+Jonathantg94@users.noreply.github.com>
2025-03-03 19:29:00 -0600

    iniciando archivos

---
+++ S3/js/nuevo.js
@@ -0,0 +1,5 @@
+// CREANDO FUCION FLECHA PARA QUITAR DUPLICADOS
+const quitarDuplicados = (arr = undefined) =>{
+    // validar si el arreglo es undefined
+    if (arr === undefined ) return console.log("no ingresaste un arreglo");
+}
